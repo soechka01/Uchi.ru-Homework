09f2b02251e8da9f199d54470fd58c355833ed2a
soechka01 <soechka01@gmail.com>
2023-08-24 05:31:27 +0400

    Lesson 6 - AI, create enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject player;
+    private NavMeshAgent _agent;
+    private Transform _target;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _target = player.transform;
+    }
+
+    void Update()
+    {
+        _agent.SetDestination(_target.position);
+
+        if ((player.transform.position - _agent.transform.position).sqrMagnitude < 10)
+        {
+            slider.value -= 10 * Time.deltaTime;
+        }
+    }
+}
