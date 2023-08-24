cd29b3b2067732b28e78909dce8c5b188e5d9676
soechka01 <soechka01@gmail.com>
2023-08-24 04:58:41 +0400

    Lesson 5 - setting ui and tags

---
+++ Assets/DangerZone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DangerZone : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Update()
+    {
+        if (slider.value == 0)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            slider.value -= 1;
+        }
+    }
+}
