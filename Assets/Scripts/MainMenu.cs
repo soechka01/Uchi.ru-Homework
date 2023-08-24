03344b52507e4cf127e48145c35dd265ee2a5b5d
soechka01 <soechka01@gmail.com>
2023-08-24 06:31:54 +0400

    Lesson 9 - main menu, end game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button exitButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        exitButton.onClick.AddListener(ExitGame);
+        
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
